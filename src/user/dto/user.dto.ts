4e8f8d523a137c62d2ae79755fdab9576b5d6311
Jobin T v <jobintv@focaloid.com>
2023-02-07 11:42:23 +0530

    feat: Open api test packaage added

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,20 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
 export class UserDto {
-  @ApiProperty({ example: 'data' })
+  @ApiProperty({ example: 1 })
   id: number;
 
-  @ApiProperty({ example: 'data' })
+  @ApiProperty({ example: 'Glenna Reicher' })
   name: string;
 
-  @ApiProperty({ example: 'data' })
+  @ApiProperty({ example: 'Delphine' })
   username: string;
 
-  @ApiProperty({ example: 'data' })
+  @ApiProperty({ example: 'Chaim_McDermott@dana.io' })
   email: string;
 
-  @ApiProperty({ example: 'data' })
+  @ApiProperty({ example: '(775)976-6794 x41206' })
   phone: string;
 
-  @ApiProperty({ example: 'data' })
+  @ApiProperty({ example: 'conrad.com' })
   website: string;
 }

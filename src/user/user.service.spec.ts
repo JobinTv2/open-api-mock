43e621dc1d5884339348a364487060f047926298
Jobin T v <jobintv@focaloid.com>
2023-02-07 14:50:10 +0530

    feat: Test for response check with users end point

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -2,21 +2,18 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import jestOpenAPI from 'jest-openapi';
 import axios from 'axios';
-import openApiJson from './openapi.json';
-
+import * as users from './users.json';
 jestOpenAPI(`D:/Training/Nest/open-api-mock/src/user/openapi.json`);
 
 describe('GET /user', () => {
   it('should satisfy OpenAPI spec', async () => {
-    // Get an HTTP response from your server (e.g. using axios)
     const res = await axios.get('http://localhost:3000/user');
-
     expect(res.status).toEqual(200);
-
-    // Assert that the HTTP response satisfies the OpenAPI spec
+    expect(res.data).toEqual(users);
     expect(res).toSatisfyApiSpec();
   });
 });
+
 describe('UserService', () => {
   let service: UserService;
 

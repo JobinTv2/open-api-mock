18647b00b89a5d31006173462eb0e765757fc013
Jobin T v <jobinvarghese18@gmail.com>
2023-02-09 18:29:04 +0530

    feat: Dynamic test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -3,7 +3,29 @@ import { UserService } from './user.service';
 import jestOpenAPI from 'jest-openapi';
 import axios from 'axios';
 import * as users from './users.json';
-jestOpenAPI(`D:/Training/Nest/open-api-mock/src/user/openapi.json`);
+import * as openAPISJson from './openapi.json';
+jestOpenAPI(`D:/Works/Training/Nest/open-api-mock/src/user/openapi.json`);
+
+console.log(openAPISJson, 'json   ');
+
+Object.entries(openAPISJson.paths).map((item) => {
+  describe('GET /user', () => {
+    it('should satisfy OpenAPI spec', async () => {
+      let path = `http://localhost:3000${item[0]}`;
+      if (path.includes(`{id}`)) path = path.replace('{id}', '1');
+      console.log(path, 'path');
+      const res = await axios.get(path);
+      expect(res.status).toEqual(200);
+      if (path === 'http://localhost:3000/user')
+        expect(res.data).toEqual(users);
+      else if (path === 'http://localhost:3000/user/1') {
+        const user = users.find((usr) => usr.id === Number(1));
+        expect(res.data).toEqual(user);
+      }
+      expect(res).toSatisfyApiSpec();
+    });
+  });
+});
 
 describe('GET /user', () => {
   it('should satisfy OpenAPI spec', async () => {
